a5f9eed1c6058a6c1952d28b1cd74b408cf50703
William Fallon <pop37corn@gmail.com>
2019-08-06 13:49:25 -0400

    small fixes

--- src/components/Menubar.js
+++ src/components/Menubar.js
@@ -3,7 +3,6 @@ import clsx from "clsx";
 import { makeStyles, useTheme } from "@material-ui/core/styles";
 import AppBar from "@material-ui/core/AppBar";
 import Toolbar from "@material-ui/core/Toolbar";
-import Typography from "@material-ui/core/Typography";
 import Drawer from "@material-ui/core/Drawer";
 import List from "@material-ui/core/List";
 import Divider from "@material-ui/core/Divider";
